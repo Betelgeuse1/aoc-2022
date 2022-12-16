f89e5b8619b7c994d310f208cd7f7a8064b1bcc2
Thomas Teixeira <thomas.teixeira@displayce.com>
2022-12-16 14:51:03 +0100

    day2: finished

--- day2/main.lua
+++ day2/main.lua
@@ -1,4 +1,5 @@
 -- Functions
+-- Part 1
 local function check(os, us)
     local d = os - us
     if math.abs(d) >= 2 then
@@ -8,12 +9,22 @@ local function check(os, us)
     return -d + 1
 end
 
-local function get_score(o, u)
-    local oscore = o - 64
-    local uscore = u - 87
+local function get_score(other, us)
+    local oscore = other - 64
+    local uscore = us - 87
 
     local c = check(oscore, uscore)
     return 3 * c + uscore
+end
+
+-- Part 2
+local function expected_score(other, strategy)
+    local oscore = other - 64
+    local sscore = strategy - 87
+    -- truth table to see why
+    local uscore = (oscore + sscore) % 3 + 1
+
+    return uscore + 3 * (sscore - 1)
 end
 
 -- Program Start
@@ -23,15 +34,15 @@ if not input then
 end
 
 local line
-local other, us
+local other, strat
 local score = 0
 while true do
     line = input:read("l")
     if not line then
         break
     end
-    other, us = line:byte(1), line:byte(3)
-    local s = get_score(other, us)
+    other, strat = line:byte(1), line:byte(3)
+    local s = expected_score(other, strat)
     score = score + s
 end
 input:close()
